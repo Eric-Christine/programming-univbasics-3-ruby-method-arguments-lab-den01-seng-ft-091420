b1f66399a5ed15c5b8b28d8632c1a46480c3f2ff
generic-state-5961 <ericjchristine@gmail.com>
2020-08-03 15:40:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my namne is #{name} and I am learning to program in #{language}"
+  puts "Hi, my namne is #{name} and I am learning to program in #{language}."
 end
 
 def introduction_with_language(name, language)
